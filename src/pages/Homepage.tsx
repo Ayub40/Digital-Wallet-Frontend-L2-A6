2a0a7dfb0b0281bd334623deeae8d1e5c9d8a483
Ayub40 <ayubk4028@gmail.com>
2025-08-22 03:34:03 +0600

    first commit

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Homepage() {
+    return (
+        <div className="container mx-auto max-w-screen-xl">
+            <Button className='bg-purple-500'>Test Button</Button>
+        </div>
+    );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import App from "@/App";
+import Homepage from "@/pages/Homepage";
+
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+    {
+        Component: App,
+        path: "/",
+        children: [
+            {
+                Component: Homepage,
+                index: true,
+            },
+        ],
+    },
+]);
