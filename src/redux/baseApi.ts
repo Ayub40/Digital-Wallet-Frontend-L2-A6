0e6bd154ea63cd74726fd99b56dd9a3fdeaa7706
Ayub40 <ayubk4028@gmail.com>
2025-08-22 19:57:27 +0600

    redux setup complete

---
+++ src/redux/baseApi.ts
@@ -0,0 +1,12 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import axiosBaseQuery from "./axiosBaseQuery";
+
+export const baseApi = createApi({
+    reducerPath: "baseApi",
+    baseQuery: axiosBaseQuery(),
+
+    // eta ( tagTypes ) dile reload na diai data cole asbe
+    // tagTypes: ["USER", "TOUR", "DIVISION", "BOOKING"],
+    tagTypes: ["USER"],
+    endpoints: () => ({}),
+});
