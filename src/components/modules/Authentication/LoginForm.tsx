3ad97d8a7605eb571e4dc18289181327951de6a3
Ayub40 <ayubk4028@gmail.com>
2025-11-04 20:43:17 +0600

    Fixed Form validations for authentication forms

--- src/components/modules/Authentication/LoginForm.tsx
+++ src/components/modules/Authentication/LoginForm.tsx
@@ -12,16 +12,33 @@ import Password from "@/components/ui/Password";
 import { cn } from "@/lib/utils";
 import { useLoginMutation } from "@/redux/features/auth/auth.api";
 import type { FetchBaseQueryError } from "@reduxjs/toolkit/query";
-import { useForm, type FieldValues, type SubmitHandler } from "react-hook-form";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Link, useNavigate } from "react-router";
 import { toast } from "sonner";
 
+
+const loginSchema = z.object({
+    email: z.string().email({ message: "Enter a valid email" }),
+    password: z.string().min(6, { message: "Password must be at least 6 characters" }),
+});
+
+type LoginFormValues = z.infer<typeof loginSchema>;
+
 export function LoginForm({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
     const navigate = useNavigate();
-    const form = useForm();
     const [login, { isLoading }] = useLoginMutation();
 
-    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const form = useForm<LoginFormValues>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    const onSubmit: SubmitHandler<LoginFormValues> = async (data) => {
         try {
             const res = await login(data).unwrap();
 
@@ -30,28 +47,22 @@ export function LoginForm({ className, ...props }: React.HTMLAttributes<HTMLDivE
                 navigate("/");
             }
         } catch (err) {
-            const typedErr = err as FetchBaseQueryError & { data?: { message?: string; errors?: any } };
-
+            const typedErr = err as FetchBaseQueryError & { data?: { message?: string } };
             console.error(typedErr);
 
-            // Field specific errors
-            if (typedErr.data?.errors) {
-                Object.keys(typedErr.data.errors).forEach((key) => {
-                    toast.error(`${typedErr.data?.errors[key]}`);
-                });
+
+            if (typedErr.data?.message === "Password does not match") {
+                toast.error("Invalid credentials");
             }
 
-            // Generic backend message
-            if (typedErr.data?.message) {
-                toast.error(typedErr.data.message);
+            if (typedErr.data?.message === "User is not verified") {
+                toast.error("Your account is not verified");
+                navigate("/verify", { state: data.email });
+            }
 
-                if (typedErr.data.message === "User is not verified") {
-                    navigate("/verify", { state: data.email });
-                }
-
-                if (typedErr.data.message === "User is BLOCKED") {
-                    navigate("/unauthorized");
-                }
+            if (typedErr.data?.message === "User is BLOCKED") {
+                toast.error("Your account is blocked");
+                navigate("/unauthorized");
             }
         }
     };
@@ -118,141 +129,3 @@ export function LoginForm({ className, ...props }: React.HTMLAttributes<HTMLDivE
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Button } from "@/components/ui/button";
-// import {
-//     Form,
-//     FormControl,
-//     FormField,
-//     FormItem,
-//     FormLabel,
-//     FormMessage,
-// } from "@/components/ui/form";
-// import { Input } from "@/components/ui/input";
-// import Password from "@/components/ui/Password";
-// import { cn } from "@/lib/utils";
-// import { useLoginMutation } from "@/redux/features/auth/auth.api";
-// import type { FetchBaseQueryError } from "@reduxjs/toolkit/query";
-// import { useForm, type FieldValues, type SubmitHandler } from "react-hook-form";
-// import { Link, useNavigate } from "react-router";
-// import { toast } from "sonner";
-
-// export function LoginForm({
-//     className,
-//     ...props
-// }: React.HTMLAttributes<HTMLDivElement>) {
-//     const navigate = useNavigate();
-//     const form = useForm();
-//     const [login] = useLoginMutation();
-
-//     const onSubmit: SubmitHandler<FieldValues> = async (data) => {
-//         try {
-//             const res = await login(data).unwrap();
-//             // console.log(res);
-
-//             if (res.success) {
-//                 toast.success("Logged in successfully");
-//                 navigate("/");
-//             }
-
-//         } catch (err) {
-//             const typedErr = err as FetchBaseQueryError & { data?: { message?: string } };
-
-//             console.error(typedErr);
-
-//             if (typedErr.data?.message === "Password does not match") {
-//                 toast.error("Invalid credentials");
-//             }
-
-//             if (typedErr.data?.message === "User is not verified") {
-//                 toast.error("Your account is not verified");
-//                 navigate("/verify", { state: data.email });
-//             }
-
-//             if (typedErr.data?.message === "User is BLOCKED") {
-//                 toast.error("Your account is blocked");
-//                 navigate("/unauthorized");
-//             }
-//         }
-//     };
-
-//     return (
-//         <div className={cn("flex flex-col gap-6", className)} {...props}>
-//             <div className="flex flex-col items-center gap-2 text-center">
-//                 <h1 className="text-2xl font-bold">Login to your account</h1>
-//                 <p className="text-balance text-sm text-muted-foreground">
-//                     Enter your email below to login to your account
-//                 </p>
-//             </div>
-//             <div className="grid gap-6">
-//                 <Form {...form}>
-//                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-//                         <FormField
-//                             control={form.control}
-//                             name="email"
-//                             render={({ field }) => (
-//                                 <FormItem>
-//                                     <FormLabel>Email</FormLabel>
-//                                     <FormControl>
-//                                         <Input
-//                                             placeholder="aaa@gmail.com"
-//                                             {...field}
-//                                             value={field.value || ""}
-//                                         />
-//                                     </FormControl>
-//                                     <FormMessage />
-//                                 </FormItem>
-//                             )}
-//                         />
-
-//                         <FormField
-//                             control={form.control}
-//                             name="password"
-//                             render={({ field }) => (
-//                                 <FormItem>
-//                                     <FormLabel>Password</FormLabel>
-//                                     <FormControl>
-//                                         <Password {...field} />
-//                                     </FormControl>
-//                                     <FormMessage />
-//                                 </FormItem>
-//                             )}
-//                         />
-
-//                         <Button type="submit" className="w-full">
-//                             Login
-//                         </Button>
-//                     </form>
-//                 </Form>
-
-//                 <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-//                     <span className="relative z-10 bg-background px-2 text-muted-foreground">
-//                         Or continue with
-//                     </span>
-//                 </div>
-//             </div>
-//             <div className="text-center text-sm">
-//                 Don&apos;t have an account?{" "}
-//                 <Link to="/register" replace className="underline underline-offset-4">
-//                     Register
-//                 </Link>
-//             </div>
-//         </div>
-//     );
-// }
